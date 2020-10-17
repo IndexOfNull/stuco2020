4423e5b676c2d60de741d421a9a936ee2d1b9532
IndexOfNull <21084775+IndexOfNull@users.noreply.github.com>
2020-10-17 13:09:20 -0500

    start using vuex and axios

--- client/src/main.js
+++ client/src/main.js
@@ -1,10 +1,32 @@
 import { createApp } from "vue";
+import { createStore } from "vuex";
 import App from "./App.vue";
 import router from "./router";
 
 import "@/assets/styles/index.scss";
 import "@/assets/styles/tailwind.css";
 
+const store = createStore({
+  state () {
+    return {
+      redirectError: "",
+      code: ""
+    }
+  },
+  mutations: {
+    error (state, err) {
+      state.redirectError = err
+    },
+    clearerror (state) {
+      state.redirectError = ""
+    },
+    code (state, code) {
+      state.code = code
+    }
+  }
+})
+
+
 createApp(App)
-  .use(router)
+  .use(router).use(store)
   .mount("#app");

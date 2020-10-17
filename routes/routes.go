de2895716e8e905d7941afbb2447c10170837b7f
IndexOfNull <21084775+IndexOfNull@users.noreply.github.com>
2020-10-17 17:16:19 -0500

    add cors, also run mod tidy

--- routes/routes.go
+++ routes/routes.go
@@ -5,9 +5,28 @@ import (
 	"github.com/indexofnull/stuco2020/controllers"
 )
 
-//SetupRouter registers all routes to their corresponding controllers
+//CORS attaches a wildcard Access-Control-Allow-Origin because gin's official package doesn't work for some dumb reason.
+//Yes, this is insecure
+func CORS() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		c.Writer.Header().Set("Access-Control-Allow-Origin", "*")
+		//c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
+		//c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With")
+		//c.Writer.Header().Set("Access-Control-Allow-Methods", "POST, OPTIONS, GET, PUT, DELETE")
+
+		/*if c.Request.Method == "OPTIONS" {
+			c.AbortWithStatus(204)
+			return
+		}*/
+
+		c.Next()
+	}
+}
+
+//SetupRouter registers all routes to their corresponding controllers as well as some middleware
 func SetupRouter() *gin.Engine {
 	r := gin.Default()
+	r.Use(CORS())
 	/*grp1 := r.Group("/api")
 	{
 		grp1.GET("ping", controllers.PongHandler)

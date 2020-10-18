5e4b05f6d56d6e61ad230a53264be30972b6df09
IndexOfNull <21084775+IndexOfNull@users.noreply.github.com>
2020-10-18 12:44:59 -0500

    add code stuff to frontend + backend model change

--- client/src/views/Vote.vue
+++ client/src/views/Vote.vue
@@ -1,9 +1,19 @@
 <template>
   <div class="h-full flex flex-col justify-center items-center">
-    <div class="sm:w-1/2 w-full">
-      <Loader></Loader>
-      <p class="text-lg">Please wait while we find your code</p>
-    </div>
+    <transition name="fade" mode="out-in">
+      <div class="sm:w-1/2 w-full" v-if="code.code == undefined">
+        <Loader></Loader>
+        <p class="text-lg">Please wait while we find your code</p>
+      </div>
+      <div v-else>
+        <h1>Hello, {{ code.student.firstname }}</h1>
+        <form>
+          <div v-for="c in classes" :key="c.id">
+            <p>{{ c.name }}</p>
+          </div>
+        </form>
+      </div>
+    </transition>
   </div>
 </template>
 
@@ -13,26 +23,60 @@ import Loader from "@/components/Loader.vue";
 
 export default {
   name: "Vote",
+  data () {
+    return {
+    code: Object,
+    response: Object,
+    classes: Object
+    }
+  },
   components: {
     Loader
   },
-  resolveCode() {
-    return false; //just for now
-  },
   beforeMount() {
-    axios
-      .get("https://api.coindesk.com/v1/bpi/currentprice.json")
-      .then(response => console.log(response)); //Just testing axios
-    setTimeout(() => {
-      this.$store.commit(
-        "error",
-        "Error! Here's proof that we're changing: " +
-          Math.trunc(Math.random() * 10) +
-          ". Also heres the code passed back from /vote: " +
-          this.$store.state.code
-      );
-      this.$router.push("/");
-    }, 2000);
+    this.$store.commit("code", this.$route.params.code) //Push the code to the store in case of direct navigation
+
+    let s = this.$store.state
+    axios.get(s.globalSettings.backendUrl + "code/" + s.code + "").then((response) => {
+
+      //Validation of code, throw error if necessary
+      if (response.data.code == null) {
+        throw 'The entered code does not exist'
+      } else if (response.data.code.times_used >= response.data.code.max_uses) {
+        throw 'The entered code has no more uses left'
+      } else if (response.data.code.active == false) {
+        throw 'The entered code is currently deactivated'
+      }
+
+      //If the code is valid
+      let data = response.data
+
+      for (const key in response.data.classes) {
+        data.classes[key].students = []
+      }
+
+      console.log(response.data)
+      data.candidates.forEach(student => {
+        if (student.class_id != undefined) {
+          data.classes[student.class_id].students.push(student)
+        }
+      })
+      this.classes = data.classes
+      this.response = response.data
+      setTimeout(() => {
+        this.code = response.data.code
+      }, 500) //Our UI is too fast otherwise lol
+    }).catch((e) => {
+      setTimeout(() => {
+        console.log(e.type)
+        if (e.message == "Network Error") {
+          this.$store.commit("error", "Something went wrong while contacting the voting service")
+        } else {
+          this.$store.commit("error", e)
+        }
+        this.$router.push("/")
+      }, 500) //Our UI is too fast otherwise lol
+    })
   }
 };
 </script>
--- models/class.go
+++ models/class.go
@@ -5,7 +5,7 @@ import "github.com/indexofnull/stuco2020/config"
 //Class represents a class. It is used to deliver the proper ballot for each student.
 type Class struct {
 	ID       uint32 `json:"id" gorm:"primaryKey;"`
-	Name     string `json:"string" gorm:"size:128;not null"`
+	Name     string `json:"name" gorm:"size:128;not null"`
 	Global   bool   `json:"global" gorm:"default:0;not null"`
 	NumVotes uint8  `json:"vote_count" gorm:"default:1;not null"`
 	//Students []Student //`gorm:"foreignKey:ClassID"`

5c0163ffa811636532bc8c31ad413f2bdd9eabb4
IndexOfNull <21084775+IndexOfNull@users.noreply.github.com>
2020-10-24 10:34:48 -0500

    add code related checks when voting

--- controllers/vote.go
+++ controllers/vote.go
@@ -40,7 +40,21 @@ func CastVote(c *gin.Context) {
 		return
 	}
 
-	for _, class := range b.Ballot { //Go through the ballot
+	//Checks about the code
+	if code.ID == 0 { //Assume that 0 ids aren't going to be a thing
+		c.JSON(http.StatusNotFound, gin.H{"error": "code does not exist"})
+		return
+	}
+	if code.Active == false {
+		c.JSON(http.StatusForbidden, gin.H{"error": "code is not active"})
+		return
+	}
+	if code.TimesUsed >= code.MaxUses {
+		c.JSON(http.StatusForbidden, gin.H{"error": "code has no more uses left"})
+		return
+	}
+
+	for _, class := range b.Ballot { //Checks about the ballot
 
 		//Deep integrity checks
 		if class.Selected == nil {
@@ -98,39 +112,3 @@ func CastVote(c *gin.Context) {
 	c.JSON(http.StatusOK, b)
 
 }
-
-/*func CastVote(c *gin.Context) {
-	var b VoteBody
-	err := c.BindJSON(&b)
-	if err != nil {
-		c.JSON(http.StatusBadRequest, err.Error())
-		return
-	}
-
-	var code models.Code
-	err = models.GetCodeByCode(&code, c.Params.ByName("code"))
-	if err != nil {
-		c.JSON(http.StatusNotFound, gin.H{"error": "error while getting code"})
-		return
-	}
-
-	if code.ID == 0 { //If the code is zero, which should not be possible with auto_increment under normal circumstances
-		c.JSON(http.StatusNotFound, gin.H{"error": "code does not exist."})
-		return
-	} else if code.TimesUsed >= code.MaxUses {
-		c.JSON(http.StatusForbidden, gin.H{"error": "code has been used too many times"})
-		return
-	} else if code.Active == false {
-		c.JSON(http.StatusForbidden, gin.H{"error": "code is not activated"})
-		return
-	} else if code.Student == nil {
-		c.JSON(http.StatusForbidden, gin.H{"error": "code is orphaned and is therefore not valid; contact the code manager to give it a new owner."})
-		return
-	}
-
-	var candidates []models.Student
-	models.GetCandidatesByIDs(&candidates, &b.IDs) //Possible DOS attack if they give us a ton of IDs?
-	fmt.Print(candidates)
-
-	//.JSON(http.StatusOK, resolved)
-}*/
